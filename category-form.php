ef140582deeffbd71dba9015e799b3af24a89a4a
Olena Stets <stetsolena7@gmail.com>
2021-02-11 23:58:50 +0100

    Edited

--- category-form.php
+++ category-form.php
@@ -1,27 +1,41 @@
 <?php
-    require_once("include/config.inc.php");
-    require_once("include/db.inc.php");
-    require_once("include/form.inc.php");
-    require_once("Class/Category.php");
-
-    $pdo = dbConnect("blog");
-
-    if (isset($_GET['addNewCategory'])) {
-        $addNewCategory = strip_tags($_GET['addNewCategory']);
-        echo "Your New Category Text: $addNewCategory<br/>";
-    }
 
 
-    if (isset($addNewCategory)) {
-        $isExists = Category::checkIfExists($pdo, $addNewCategory);
+#***************************************************************************************#
 
-        if ($isExists) {
-            echo "This category is already available!";
-        } else {
-            Category::saveToDb($pdo, $addNewCategory);
-            echo "<h1>New category was added to database!</h1>";
+				#***********************************#
+				#********** CONFIGURATION **********#
+				#***********************************#
 
-            header("Location: dashboard.php");
-        }
-    }
+                require_once("include/config.inc.php");
+                require_once("include/db.inc.php");
+                require_once("include/form.inc.php");
+                require_once("Class/Category.php");
+
+                #********** ESTABLISH DB CONNECTION **********#	
+
+                $pdo = dbConnect("blog");
+
+                #************ CHECK WHETHER PARMETER WAS PASSED *****************#
+
+                if (isset($_GET['addNewCategory'])) {
+                    $addNewCategory = strip_tags($_GET['addNewCategory']);
+                    echo "Your New Category Text: $addNewCategory<br/>";
+                }
+
+                if (isset($addNewCategory)) {
+                    $isExists = Category::checkIfExists($pdo, $addNewCategory);
+
+                    if ($isExists) {
+                        echo "This category is already available!";
+                    } else {
+                        Category::saveToDb($pdo, $addNewCategory);
+                        echo "<h1>New category was added to database!</h1>";
+
+                        header("Location: dashboard.php");
+                    }
+                }
+
+#***************************************************************************************#
+
 ?>
